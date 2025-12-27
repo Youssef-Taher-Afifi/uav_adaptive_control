a514d4f436117e9f4b9d643580daa08f269d174e
Youssef Taher <yousef_taher@yahoo.com>
2025-12-27 14:25:20 +0200

    Updated trajectory publisher with 3D square trajectory

--- uav_adaptive_control/trajectory_publisher.py
+++ uav_adaptive_control/trajectory_publisher.py
@@ -9,31 +9,50 @@ class TrajectoryPublisher(Node):
         super().__init__("trajectory_publisher")
         self.pose_pub = self.create_publisher(PoseStamped, "/reference_pose", 10)
         self.twist_pub = self.create_publisher(TwistStamped, "/reference_twist", 10)
+        
         self.t = 0.0
-        self.Tf = 20.0
+        self.Tf = 20.0  # Total time for one full loop
         self.dt = 0.01
         self.timer = self.create_timer(self.dt, self.update)
 
-        # start/end points
-        self.p0 = np.array([0., 0., 1.])
-        self.pf = np.array([4., 4., 2.])
-
-        # precompute 5th-order minimal-jerk coefficients
-        self.a0 = self.p0
-        self.a1 = np.zeros(3)
-        self.a2 = np.zeros(3)
-        self.a3 = 10*(self.pf - self.p0)/(self.Tf**3)
-        self.a4 = -15*(self.pf - self.p0)/(self.Tf**4)
-        self.a5 = 6*(self.pf - self.p0)/(self.Tf**5)
+        # Define the 4 corners of the square (Tilted in Z)
+        # Order: Top-Right -> Top-Left -> Bottom-Left -> Bottom-Right
+        # Z is calculated as z = 0.25*x + 0.25*y + 2.0 to ensure it is a flat tilted plane
+        self.waypoints = [
+            np.array([ 2.0,  2.0, 3.0]), # Corner 1
+            np.array([-2.0,  2.0, 2.0]), # Corner 2
+            np.array([-2.0, -2.0, 1.0]), # Corner 3
+            np.array([ 2.0, -2.0, 2.0])  # Corner 4
+        ]
+        self.num_segments = len(self.waypoints)
+        self.segment_time = self.Tf / self.num_segments
 
     def update(self):
-        if self.t > self.Tf:
-            self.t = 0.0
+        # 1. Handle time wrapping
+        if self.t >= self.Tf:
+            self.t -= self.Tf
 
-        tt = np.array([self.t**i for i in range(6)])
-        pos = self.a0 + self.a3*tt[3] + self.a4*tt[4] + self.a5*tt[5]
-        vel = 3*self.a3*tt[2] + 4*self.a4*tt[3] + 5*self.a5*tt[4]
+        # 2. Determine which segment we are in (0 to 3)
+        # current_idx is the start corner, next_idx is the destination
+        current_idx = int(self.t // self.segment_time)
+        next_idx = (current_idx + 1) % self.num_segments
 
+        # 3. Calculate local time within the segment (0.0 to segment_time)
+        t_local = self.t % self.segment_time
+        
+        # 4. Get Start and End points for this segment
+        p_start = self.waypoints[current_idx]
+        p_end   = self.waypoints[next_idx]
+
+        # 5. Linear Interpolation (Position)
+        # pos = start + (direction * percentage_complete)
+        pos = p_start + (p_end - p_start) * (t_local / self.segment_time)
+
+        # 6. Constant Velocity for this segment
+        # vel = distance / time
+        vel = (p_end - p_start) / self.segment_time
+
+        # --- Construct Messages ---
         msg_pos = PoseStamped()
         msg_pos.header.stamp = self.get_clock().now().to_msg()
         msg_pos.pose.position.x = float(pos[0])
